b2b60eb43f3d817e6ba943adee0869df502c8958
Arthur Vinicius <thuur.vss@gmail.com>
2022-03-29 08:58:25 -0300

    Mostrando no relógio

--- src/components/Cronometro/Relogio/index.tsx
+++ src/components/Cronometro/Relogio/index.tsx
@@ -1,13 +1,21 @@
 import "./Relogio.scss";
 
-export default function Relogio(){
+interface Props {
+    tempo: number | undefined;
+}
+
+export default function Relogio({ tempo = 0 }: Props) {
+    const minutos = Math.floor(tempo / 60);
+    const [minutoDezena, minutoUnidade] = String(minutos).padStart(2, "0")
+    const segundos = (tempo % 60);
+    const [segundoDezena, segundoUnidade] = String(segundos).padStart(2, "0")
     return(
         <>
-        <span className="relogioNumero">0</span>
-        <span className="relogioNumero">0</span>
+        <span className="relogioNumero">{minutoDezena}</span>
+        <span className="relogioNumero">{minutoUnidade}</span>
         <span className="relogioDivisao">:</span>
-        <span className="relogioNumero">0</span>
-        <span className="relogioNumero">0</span>
+        <span className="relogioNumero">{segundoDezena}</span>
+        <span className="relogioNumero">{segundoUnidade}</span>
         </>
     )
 }
--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -19,9 +19,8 @@ export default function Cronometro({ selecionado }: Props) {
     return(
         <div className="cronometro">
             <p className="titulo">Escolha um Card e inicie o cronômetro:</p>
-            Tempo:{tempo}
             <div className="relogioWrapper">
-                <Relogio />
+                <Relogio tempo={tempo}/>
             </div>
             <Botao>
                 Começar

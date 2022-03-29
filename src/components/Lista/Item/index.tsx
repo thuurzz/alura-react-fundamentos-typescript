6ba663af08e45a7a8cdbd08b575627efdef05a02
Arthur Vinicius <thuur.vss@gmail.com>
2022-03-29 09:34:28 -0300

    Finalizando a tarefa

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -8,11 +8,14 @@ interface Props extends ITarefa{
 
 export default function Item({ tarefa, tempo, selecionado, completado, id , selecionaTarefa }: Props) {
     return(
-        <li className={`${"item"} ${selecionado ? "itemSelecionado" : ""}`} onClick={() => selecionaTarefa({
+        <li className={`
+            ${"item"} ${selecionado ? "itemSelecionado" : ""} ${completado ? "itemCompletado" : ""}
+        `} onClick={() => !completado && selecionaTarefa({
             tarefa, tempo, selecionado, completado, id
         })}>
             <h3>{tarefa}</h3>
             <span>{tempo}</span>
+            {completado && <span className='concluido' aria-label="tarefa completada"></span>}
         </li>
     )
 }
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -18,6 +18,23 @@ function App() {
     })))
   }
 
+  function finalizarTarefa() {
+    if (selecionado) {
+      setSelecionado(undefined);
+      setTarefas(tarefasAnteriores =>
+        tarefasAnteriores.map(tarefa => {
+        if (tarefa.id === selecionado.id) {
+          return {
+            ...tarefa,
+            selecionado: false,
+            completado: true
+          }
+        }
+        return tarefa;
+      }))
+    }
+  }
+
   return (
     <div className={style.AppStyle}>
       <Formulario setTarefas={ setTarefas }/>
@@ -25,7 +42,10 @@ function App() {
         tarefas={tarefas}
         selecionaTarefa={selecionaTarefa}
       />
-      <Cronometro selecionado={selecionado}/>
+      <Cronometro
+        selecionado={selecionado}
+        finalizarTarefa={finalizarTarefa}
+      />
     </div>
   );
 }
